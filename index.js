b37448b14151917db69e2ec7722e116be815fc01
Anisha Gupta <ani6gup@gmail.com>
2018-07-30 00:43:58 -0700

    Added index.html

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World! New Info! Another 1! One more!')
+ response.sendFile(__dirname + '/views/index.html');
 })
 
 app.listen(app.get('port'), function() {
